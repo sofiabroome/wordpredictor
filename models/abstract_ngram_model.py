203f16e92f226cf30571000c2cafeca9dd9652b6
Jeremy KREBS <jeremy.krebs@pricematch.travel>
2015-10-12 14:51:21 +0200

    Add prediction based on the probability distribution of ngrams

--- models/abstract_ngram_model.py
+++ models/abstract_ngram_model.py
@@ -1,3 +1,4 @@
+import random
 
 """Abstract Class to represent a ngram model implementation"""
 
@@ -11,4 +12,19 @@ class AbstractNgramModel():
         raise NotImplementedError
 
     def predict_words(self, base_query_string, n=100):
-        raise NotImplementedError
+        matches = []
+        for i in range(n):
+            matches = self.predict_next_word(base_query_string)
+            weight_sum = sum(m[-1] for m in matches)
+            norm_matches = [(m[0], m[-1] / weight_sum) for m in matches]
+            rand = random.random()
+            i = 0
+            if len(norm_matches) != 0:
+                while (rand > norm_matches[i][-1]):
+                    rand -= norm_matches[i][-1]
+                    i += 1
+                base_query_string = base_query_string + " " + norm_matches[i][0][-1]
+            else:
+                print "NO_MORE_OPTION"
+        print "The result is:\n", base_query_string
+        return base_query_string
